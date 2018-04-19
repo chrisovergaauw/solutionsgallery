551beff4010e5be3f9f1822809b64fe3fecaf199
Chris Overgaauw <14801878+chrisovergaauw@users.noreply.github.com>
2018-04-20 01:11:50 +0200

    now able to go fullscreen or save canvas on 1 sketch. added description for said sketch
    made html imports more readable

--- challenges/dots/sketch.js
+++ challenges/dots/sketch.js
@@ -1,17 +1,32 @@
+
+
 var starSketch = function(sketch) {
 
-    sketch.setup = function() {
+    let getSketchControls = "f = fullscreen, s = save canvas to PNG";
+
+    sketch.setup = function () {
         var parentDiv = document.getElementById('jumboid');
-        var pwidth = parentDiv.offsetWidth*0.95;
-        var pheight = parentDiv.offsetHeight*0.95;
-        console.log(pheight);
+        var pwidth = parentDiv.offsetWidth * 0.95;
+        var pheight = parentDiv.offsetHeight * 0.95;
         sketch.createCanvas(pwidth, pheight);
         sketch.background(0);
+        this.p = sketch.createP(getSketchControls);
+        this.p.addClass('lead');
+        this.p.addClass('text-muted');
     };
 
-    sketch.draw = function() {
+    sketch.draw = function () {
         //sketch.fill();
         sketch.stroke(255);
-        sketch.ellipse(sketch.random(sketch.width), sketch.random(sketch.height), 10, 10 );
+        sketch.ellipse(sketch.random(sketch.width), sketch.random(sketch.height), 10, 10);
+    };
+
+    sketch.keyPressed = function (k) {
+        if (k.key === 'f') {
+            sketch.fullscreen(true);
+        }
+        if (k.key === 's') {
+            sketch.save('thumbnail.png');
+        }
     };
 };
